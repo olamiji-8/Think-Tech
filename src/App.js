34185d899ca8da71beac0eb9abd2972a2616948f
olamiji-8 <olamijisotomi@gmail.com>
2023-03-17 12:34:28 +0100

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Navbar from './Component/Navbar/Navbar'
+import Home from './Component/Home/Home'
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      
+      <Home/>
+
     </div>
   )
 }
