54341fbab203e361423630dede65ced9b36a1aa4
olamiji-8 <olamijisotomi@gmail.com>
2023-03-17 10:58:15 +0100

    update

---
+++ src/Component/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ThinkTech from "../../Assets/ThinkTech_logo 1.svg"
+
+const Navbar = () => {
+  return (
+    <div>
+       <img src={ThinkTech} alt='' />
+    </div>
+  )
+}
+
+export default Navbar
