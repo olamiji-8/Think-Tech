cefdf76986d0a53c97b0fbb0b9a1b512f95d0921
olamiji-8 <olamijisotomi@gmail.com>
2023-03-17 18:31:10 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Home from './Component/Home/Home'
+import Schedule from './Component/Schedule/Schedule'
 
 const App = () => {
   return (
     <div>
       <Home/>
+      <Schedule/>
 
     </div>
   )
