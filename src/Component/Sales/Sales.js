d4a0170888c1996cb5e1ef83aa5861f1fbeba20e
olamiji-8 <olamijisotomi@gmail.com>
2023-03-18 23:36:43 +0100

    Update

--- src/Component/Sales/Sales.js
+++ src/Component/Sales/Sales.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Sales = () => {
   return (
-    <div>Sales</div>
+    <div>
+        <p>Merch sales</p>
+        <div>
+            <p>Don't miss out on the opportunity to get your hands on exclusive Think Tech merchandise! Show your love for technology and join the hype by purchasing our limited edition t-shirts, mugs, and more. Stock is running low, so act fast and secure your gear before it's too late!</p>
+            <img src='' alt=''/>
+        </div>
+        <button>Buy Merchandise</button>
+    </div>
   )
 }
 
