cf39ceedec56783dd48c986f3a869327a115dacb
olamiji-8 <olamijisotomi@gmail.com>
2023-03-18 23:12:43 +0100

    Speakers

--- src/Component/Speakers/Speakers.js
+++ src/Component/Speakers/Speakers.js
@@ -4,7 +4,18 @@ import "./Speakers.css"
 const Speakers = () => {
   return (
     <div className='Speakers'>
+        {
+            Speakers?.map((Speakers)=>(
+                <div key={Speakers.id}>
+                   { <div> <img src={Speakers.images} alt="#" width="80%" height="40%" className='product__img'/> </div>}  
+                   <div>
+                    <h6>{Speakers.name}</h6>
+                    <h6>{Speakers.post}</h6>
+                   </div>
+                </div>
 
+            ))}
+        
     </div>
   )
 }
