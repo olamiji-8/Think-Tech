6d91fa5f74f70940fa1c8f0bfe1a57299e6963ed
olamiji-8 <olamijisotomi@gmail.com>
2023-03-18 23:39:25 +0100

    update

--- src/Component/Sales/Sales.js
+++ src/Component/Sales/Sales.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Cloth from "../../Assets/Tee 1.svg"
+import "./Sales.css"
 
 const Sales = () => {
   return (
@@ -6,7 +8,7 @@ const Sales = () => {
         <p>Merch sales</p>
         <div>
             <p>Don't miss out on the opportunity to get your hands on exclusive Think Tech merchandise! Show your love for technology and join the hype by purchasing our limited edition t-shirts, mugs, and more. Stock is running low, so act fast and secure your gear before it's too late!</p>
-            <img src='' alt=''/>
+            <img src={Cloth} alt=''/>
         </div>
         <button>Buy Merchandise</button>
     </div>
