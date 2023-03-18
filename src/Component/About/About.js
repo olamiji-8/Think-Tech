c1fb243632e08576435352acc583b7ffd2f19271
olamiji-8 <olamijisotomi@gmail.com>
2023-03-18 22:46:20 +0100

    update

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import AboutImg from "../../Assets/amico.svg"
+import "./About.css"
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+        <p>About Us</p>
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur. Bibendum ultrices amet commodo donec quis.</p>
+            <img src={AboutImg} alt=""/>
+        </div>
+        <div>
+            <p>Think Tech is a tech event organised by the ICT Team of RCFFUTA targeted at individuals around FUTA with an interest.</p>
+            <button>Get Started Here</button>
+        </div>
+    </div>
   )
 }
 
