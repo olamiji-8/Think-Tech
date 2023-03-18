901c7d83fd9d38a88f97df9d8a821f63feee6066
olamiji-8 <olamijisotomi@gmail.com>
2023-03-18 22:55:48 +0100

    Data

---
+++ src/Data.js
@@ -0,0 +1,33 @@
+const Speaker = [
+  {
+    id: 1,
+    img: "https://firebasestorage.googleapis.com/v0/b/clone-b643d.appspot.com/o/Amazon%20gift%20card%2Fe1.jpg?alt=media&token=9cc56e8d-43b1-4746-8829-61463bada326",
+    name: "Walker",
+    post: "CEO",
+  },
+  {
+    id: 2,
+    img: "https://firebasestorage.googleapis.com/v0/b/clone-b643d.appspot.com/o/Amazon%20gift%20card%2Fe1.jpg?alt=media&token=9cc56e8d-43b1-4746-8829-61463bada326",
+    name: "Walker",
+    post: "CEO",
+  },
+  {
+    id: 3,
+    img: "https://firebasestorage.googleapis.com/v0/b/clone-b643d.appspot.com/o/Amazon%20gift%20card%2Fe1.jpg?alt=media&token=9cc56e8d-43b1-4746-8829-61463bada326",
+    name: "Walker",
+    post: "CEO",
+  },
+  {
+    id: 4,
+    img: "https://firebasestorage.googleapis.com/v0/b/clone-b643d.appspot.com/o/Amazon%20gift%20card%2Fe1.jpg?alt=media&token=9cc56e8d-43b1-4746-8829-61463bada326",
+    name: "Walker",
+    post: "CEO",
+  },
+  {
+    id: 5,
+    img: "https://firebasestorage.googleapis.com/v0/b/clone-b643d.appspot.com/o/Amazon%20gift%20card%2Fe1.jpg?alt=media&token=9cc56e8d-43b1-4746-8829-61463bada326",
+    name: "Walker",
+    post: "CEO",
+  },
+];
+export default Speaker;
