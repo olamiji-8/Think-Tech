23b22ab8bdceaa2a86f0b79ae071a57029cbda05
olamiji-8 <olamijisotomi@gmail.com>
2023-03-18 23:45:32 +0100

    Courses

---
+++ src/Component/Courses/Courses.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Courses.css"
+
+const Courses = () => {
+  return (
+    <div className='Courses'>
+        <p>Our Courses</p>
+        <div>
+            <p>Major Courses</p>
+        </div>
+    </div>
+  )
+}
+
+export default Courses
