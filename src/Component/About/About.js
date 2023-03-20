12b08d51a0a79fddfe8562bbfafe6d266916bd71
olamiji-8 <olamijisotomi@gmail.com>
2023-03-20 16:31:50 +0100

    update

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -6,13 +6,15 @@ const About = () => {
   return (
     <div className='About'>
         <p className='About_text'>ABOUT US</p>
-        <div>
-            <p>Lorem ipsum dolor sit amet consectetur. Bibendum ultrices amet commodo donec quis.</p>
-            <img src={AboutImg} alt=""/>
+        <div className='About_flex'>
+        <div className='About_grid'>
+            <p className='About_para'>Lorem ipsum dolor sit amet consectetur. Bibendum ultrices amet commodo donec quis.</p>
+            <p className='About_subpara'>Think Tech is a tech event organised by the ICT Team of RCFFUTA targeted at individuals around FUTA with an interest.</p>
+            <button className='About_button'>Get Started Here</button>
         </div>
-        <div>
-            <p>Think Tech is a tech event organised by the ICT Team of RCFFUTA targeted at individuals around FUTA with an interest.</p>
-            <button>Get Started Here</button>
+        <div className='About_img'>
+        <img src={AboutImg} alt=""/>
+        </div>
         </div>
     </div>
   )
