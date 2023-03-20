9d2530222cba866a75a591ee7f68997bdeff24c6
olamiji-8 <olamijisotomi@gmail.com>
2023-03-20 11:20:33 +0100

    Data updated

--- src/Data.js
+++ src/Data.js
@@ -52,3 +52,30 @@ export const Testimony =[
     post:"Brand and Business Coach"
   }
 ]
+
+export const Questions =[
+  {
+    id:1,
+    question1:"I don’t have a laptop can I attend?",
+  },
+  {
+    id:2,
+    question2:"I am not a member of RCF, can I attend?",
+  },
+  {
+    id:3,
+    question3:"I am not a student of FUTA, can I attend?",
+  },
+  {
+    id:4,
+    question4:"Lorem ipsum dolor sit amet cons ectetur?",
+  },
+  {
+    id:5,
+    question5:"Lorem ipsum dolor sit amet cons ectetur?",
+  },
+  {
+    id:6,
+    question6:"Lorem ipsum dolor sit amet cons ectetur?",
+  }
+]
