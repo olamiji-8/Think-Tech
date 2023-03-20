566be0e12b88746aeb0a0f2ce4b80cc40e43a9f9
olamiji-8 <olamijisotomi@gmail.com>
2023-03-20 11:35:41 +0100

    updated

--- src/Component/Testimony/testimony.js
+++ src/Component/Testimony/testimony.js
@@ -8,7 +8,41 @@ const Testimony = () => {
       <div>
         <p>Frequently Asked Questions</p>
         <div>
-          
+
+        </div>
+      </div>
+      <div>
+        <p>Register Here</p>
+        <div>
+          <div>
+            <img src='' alt=""/>
+            <img src='' alt=""/>
+          </div>
+          <div>
+            <p>Name</p>
+            <input type="text"/>
+            <p>Email</p>
+            <input type="text"/>
+            <p>Phone Number</p>
+            <input type="number"/>
+            <p>What Stack Are You Interested In?</p>
+            <div>
+              <p>Mobile Deveopment</p>
+              <p>Data Analysis</p>
+            </div>
+            <div>
+              <p>Do You Have Any Knowledge Of The Stack?</p>
+              <div>
+                <p>Beginner</p>
+                <p>Intermediate </p>
+              </div>
+            </div>
+            <div>
+              <p>How Did You Hear About Us?</p>
+              <input type="text"/>
+            </div>
+            <button>Submit</button>
+          </div>
         </div>
       </div>
     </div>
