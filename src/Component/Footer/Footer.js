af891a107c6ffcddbe205ddbd8edf93c1db1e929
olamiji-8 <olamijisotomi@gmail.com>
2023-03-22 04:45:56 +0100

    update

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import FooterImg from "../../Assets/Footer.svg"
+import FooterImg from "../../Assets/ThinkTech_logo 1.svg"
 import "./Footer.css"
 
 const Footer = () => {
   return (
     <div className='Footer'>
         <img src={FooterImg} alt=""/>
+        <p>Business * Data * Dev</p>
         <p>© 2023. All rights reserved</p>
     </div>
   )
