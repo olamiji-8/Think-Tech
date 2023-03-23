f504a4a4ff492870c17b9e5c224841ea8567296d
olamiji-8 <olamijisotomi@gmail.com>
2023-03-23 10:42:15 +0100

    update

--- src/Component/Testimony/testimony.js
+++ src/Component/Testimony/testimony.js
@@ -6,13 +6,13 @@ const Testimony = () => {
   return (
     <div className='Testimony'>
       <p className='Testimony_text'>TESTIMONIALS</p>
-      <div>
+      <div className='Testimony_flex'>
       {Testi.map(Info =>{
         return(
-          <div>
-            <p>{Info.testimony}</p>
-            <p>{Info.name}</p>
-            <p>{Info.post}</p>
+          <div className='Testimony_border'>
+            <p className='Testimonytext'>{Info.testimony}</p>
+            <p className='Testimonyname'>{Info.name}</p>
+            <p className='Testimonypost'>{Info.post}</p>
           </div>
         )
       })}
