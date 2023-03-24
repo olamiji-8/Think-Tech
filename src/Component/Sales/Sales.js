49613ea03d2c6b43ddfe7dd6e8898a8a66e5fa64
olamiji-8 <olamijisotomi@gmail.com>
2023-03-24 18:31:28 +0100

    update

--- src/Component/Sales/Sales.js
+++ src/Component/Sales/Sales.js
@@ -7,7 +7,7 @@ const Sales = () => {
     <div className='Sales'>
         <p className='Sales_text'>MERCH SALES</p>
         <div className='Sales_flex'>
-            <p className='Sales_textp'>Get the Thinktech 3.0 merch @ N3200. Order yours now and join the Thinktech revolution in style! Click the link below to order.</p>
+            <p className='Sales_textp'>Get the <span style={{color:"#157EBF"}}> Thinktech 3.0 </span> merch @ <span style={{color:"#F3A310"}}> N3200 </span>. Order yours now and join the Thinktech revolution in style! Click the link below to order.</p>
             <img src={Cloth} alt='' style={{marginLeft:"4%"}}/>
         </div>
         <button className='Sales_button'>Buy Merchandise</button>
