c9df94aa6535898f792ed54fa565775e32ad5e65
olamiji-8 <olamijisotomi@gmail.com>
2023-03-24 14:43:07 +0100

    update

--- src/Component/Schedule/Schedule.js
+++ src/Component/Schedule/Schedule.js
@@ -4,46 +4,7 @@ import "./Schedule.css"
 const Schedule = () => {
   return (
     <div className='Schedule'>
-       <p className='Schedule_text'>SCHEDULE</p>
-       <div className='Schedule_text_session'>
-        <p>MORNING SESSION</p>
-        <p>AFTERNOON SESSION</p>
-       </div>
-       <div>
-        <p>DAY ONE</p>
-        <p>DAY TWO</p>
-       </div>
-       <div>
-        <p>WhatsApp Business</p>
-        <p>Meta Business</p>
-       </div>
-       <div>
-        <div>
-          <p>Breakout Session 1</p>
-          <p>Mobile Development & Data Analysis</p>
-        </div>
-        <div>
-          <p>Breakout Session 2</p>
-          <p>Mobile Development & Data Analysis</p>
-        </div>
-       </div>
-       <div>
-        <p>Mobile Development & Data Analysis</p>
-       </div>
-       <div>
-        <p>NFT</p>
-       </div>
-       <div>
-        <p>Personal Branding</p>
-       </div>
-       <div>
-        <p>Workshop 1</p>
-        <p>Mobile Development & Data Analysis</p>
-       </div>
-       <div>
-        <p>Workshop 2</p>
-        <p>Mobile Development & Data Analysis</p>
-       </div>
+       
     </div>
   )
 }
