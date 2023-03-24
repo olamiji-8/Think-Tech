337c8d17f0403cc34546fb48f760881abdebe7c8
olamiji-8 <olamijisotomi@gmail.com>
2023-03-24 23:36:58 +0100

    update

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
     <div className='Home'>
         <Navbar/>
         <img src={Hometext} alt="" style={{marginLeft:"10%", marginTop:"10%"}}/>
-        <p className='Home_text'>Register to participate in Think Tech 3.0 and gain access to free courses, games, and lots more.</p>
+        <p className='Home_text'>There's room for you in the tech ecosystem. Join us to kickstart your tech journey with the right information and in-demand skills.</p>
         <button className='Home_button'>Register</button>
     </div>
   )
--- src/Component/Schedule/Schedule.js
+++ src/Component/Schedule/Schedule.js
@@ -4,7 +4,10 @@ import "./Schedule.css"
 const Schedule = () => {
   return (
     <div className='Schedule'>
-       
+       <p className='Schedule_text'>
+       Think Tech is a yearly 2-day event that's held to teach students Tech skills, to either help them either kick-start their Tech journey or take them a step further in it. This year, we're coaching students majorly on Data Analysis and Mobile Development.
+
+       </p>
     </div>
   )
 }
