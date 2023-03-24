4864a327b54a2354490d2e6f9765d9ba092dfbcd
olamiji-8 <olamijisotomi@gmail.com>
2023-03-24 23:51:51 +0100

    update

--- src/Component/Speakers/Speakers.js
+++ src/Component/Speakers/Speakers.js
@@ -12,13 +12,10 @@ const Speakers = () => {
           <div className='Speakers_grid' key={Data.id }>
             <div className='Speakers_flex'>
             <img src={Data.img} alt="" width="40%" height="40%" className='product__img'/>
-            <div className='Speakers_textp'>
-            <h2>{Data.name}</h2>
-            <h5>{Data.post}</h5>
+            <p>{Data.name}</p>
+            <p>{Data.post}</p>
             </div>
             </div>
-            
-        </div>
         )
       })}
 </div>
