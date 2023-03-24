ae737b02c9074bbac0946b2f10933cf2a4200d7f
olamiji-8 <olamijisotomi@gmail.com>
2023-03-24 17:52:13 +0100

    update data

--- src/Data.js
+++ src/Data.js
@@ -79,3 +79,43 @@ export const Questions =[
     question:"Lorem ipsum dolor sit amet cons ectetur?",
   }
 ]
+
+export const Courses =[
+  {
+    id:1,
+    title:"Introduction to Mobile Development -  using Flutter",
+    description:"Enim arcu fringilla donec arcu fames donec massa quis. Nisi consectetur urna dignissim et rhoncus augue dolor."
+  },
+  {
+    id:2,
+    title:"Introduction to Mobile Development -  using Flutter",
+    description:"Enim arcu fringilla donec arcu fames donec massa quis. Nisi consectetur urna dignissim et rhoncus augue dolor."
+  }
+]
+
+export const Subcourses =[
+  {
+    id:1,
+    img:"",
+    title:"Meta Business",
+    description:"Manage your Facebook and Instagram marketing on Meta Business Suite for optimal business growth and customer engagement."
+  },
+  {
+  id:2,
+  img:"",
+  title:"Understanding NFTS and Blockchain",
+  description:"Discover the basics of NFTs and blockchain technology, including how NFTs use the blockchain to enhance security and transparency."
+},
+{
+  id:3,
+  img:"",
+  title:"WhatsApp Business",
+  description:"Maximize your sales and exposure using WhatsApp Business. Learn how to optimize your profile, create catalogs, and more."
+},
+{
+  id:4,
+  img:"",
+  title:"Personal Branding",
+  description:"Develop a strong and consistent brand identity with the use of brand persona, and other related strategies."
+}
+]
