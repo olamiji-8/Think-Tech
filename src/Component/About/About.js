61f76e520f48f5b1f962ae1b5bea8a41c1e44501
olamiji-8 <olamijisotomi@gmail.com>
2023-03-24 13:41:30 +0100

    update

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -7,14 +7,15 @@ const About = () => {
     <div className='About'>
         <p className='About_text'>ABOUT US</p>
         <div className='About_flex'>
+        <div className='About_img'>
+        <img src={AboutImg} alt=""/>
+        </div>
         <div className='About_grid'>
             <p className='About_para'>Register to participate in Think Tech 3.0</p>
             <p className='About_subpara'>and gain access to free courses, games, and lots more. Register to participate in Think Tech 3.0 and gain access to free courses, games, and lots more. Register to participate in Think Tech 3.0 and gain access to free courses, games, and lots more.</p>
             <button className='About_button'>Get Started Here</button>
         </div>
-        <div className='About_img'>
-        <img src={AboutImg} alt=""/>
-        </div>
+
         </div>
     </div>
   )
