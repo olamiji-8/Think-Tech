4cabe7c2531d404300b4dd31b1114284d6084d84
olamiji-8 <olamijisotomi@gmail.com>
2023-03-25 09:46:10 +0100

    update

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import FooterImg from "../../Assets/ThinkTech_logo 1.svg"
+import FooterImg from "../../Assets/ThinkTech_logo 1 (1).svg"
 import "./Footer.css"
 
 const Footer = () => {
   return (
     <div className='Footer'>
       <div className='Footer_top'>
-        <img src={FooterImg} alt="" style={{marginTop:"8%"}}/>
-        <p className='Footer_text'>Business * Data * Dev</p>
+        <img src={FooterImg} alt="" className='Footer_img' style={{marginTop:"8%"}}/>
         <p className='Footer_textp'>© 2023. All rights reserved</p>
     </div>
     </div>
