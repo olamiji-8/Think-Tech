69ab79aab8d858a28a59206a6e1fe4b6d2f3dea1
olamiji-8 <olamijisotomi@gmail.com>
2023-03-25 13:16:29 +0100

    update schedule

--- src/Component/Schedule/Schedule.js
+++ src/Component/Schedule/Schedule.js
@@ -6,8 +6,8 @@ const Schedule = () => {
     <div className='Schedule'>
        <p className='Schedule_text'>
        Think Tech is a yearly 2-day event that's held to teach students Tech skills, to either help them either kick-start their Tech journey or take them a step further in it. This year, we're coaching students majorly on Data Analysis and Mobile Development.
-
        </p>
+       <button className='Schedule_started'>Get Started Here</button>
        <div className='Schedule_border'>
 
        </div>
