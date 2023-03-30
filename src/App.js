e89015e2592f965e95aea1813daafe0568567283
olamiji-8 <olamijisotomi@gmail.com>
2023-03-30 01:01:34 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import React from 'react'
-import "./index.css"
-import About from './Component/About/About'
-import Courses from './Component/Courses/Courses'
-import Footer from './Component/Footer/Footer'
-import Home from './Component/Home/Home'
-import Partners from './Component/Partners/Partners'
-import Sales from './Component/Sales/Sales'
-import Schedule from './Component/Schedule/Schedule'
-import Speakers from './Component/Speakers/Speakers'
-import Register from './Component/Testimony/Register'
-import Question from './Component/Testimony/Question'
-import Testimony from './Component/Testimony/Testimony'
+import React from "react";
+import "./index.css";
+import About from "./Component/About/About";
+import Courses from "./Component/Courses/Courses";
+import Footer from "./Component/Footer/Footer";
+import Home from "./Component/Home/Home";
+import Sales from "./Component/Sales/Sales";
+import Schedule from "./Component/Schedule/Schedule";
+import Register from "./Register/Register";
+import Question from "./Question/Question";
+
+import Testimony from "./Testimony/Testimony";
 
 const App = () => {
   return (
     <div>
-      <Home/>
-      <Schedule/>
-      <About/>
-      <Courses/>
-      <Testimony/>
-      <Register/>
-      <Sales/>
-      <Question/>
-      <Footer/>
+      <Home />
+      <Schedule />
+      <About />
+      <Courses />
+      <Testimony />
+      <Register />
+      <Sales />
+      <Question />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/Question/Question.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Questions } from "../Data";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import Register from "../Register/Register";
+import "./Testimony.css";
+const Question = () => {
+  return (
+    <div className="Question">
+      <p className="Testimony_question">Frequently Asked Questions</p>
+      <div className="Questions_flex">
+        {Questions.map((Info) => {
+          return (
+            <div className="Questions_border">
+              <p className="Questions_text">{Info.question}</p>
+              <div className="Drop_downborder">
+                <div className="Drop_down">
+                  <ArrowDropDownIcon />
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <Register />
+    </div>
+  );
+};
+
+export default Question;
