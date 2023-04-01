81d4cbb5fd650b6fe61d80f55b32e6909ecb4f41
olamiji-8 <olamijisotomi@gmail.com>
2023-04-01 13:28:59 +0100

    updated

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -2,19 +2,25 @@ import React from 'react'
 import "./Navbar.css"
 import ThinkTech from "../../Assets/ThinkTech_logo 1 (1).svg"
 import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className='Navbar'>
        <img src={ThinkTech} alt=''  className="Navbar_img"/>
        <div className='Navbar_text'>
+       {/* <Link to="/"><p>About</p></Link>
+       <Link to="/"><p>Courses</p></Link>
+       <Link to="/"><p>Speakers</p></Link>
+       <Link to="/"><p>Speakers</p></Link>
+       <Link to="/"><p>Shop</p></Link>
+       <Link to="/"><button className="Navbar_button">Register</button></Link> */}
        
-       <p>About</p>
-       <p>Courses</p>
-       <p>Speakers</p>
-       <p>Speakers</p>
-       <p>Shop</p>
-       <button className="Navbar_button">Register</button>
+       
+       
+       
+       
+       
        </div>
     </div>
   )
