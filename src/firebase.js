fc0f6e0da889ed525376b4758b6bc6bd6c6f7367
olamiji-8 <olamijisotomi@gmail.com>
2023-04-02 04:29:33 +0100

    update

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,14 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBxS8ZVCLfE059BOwj6FaOkzsKkjwr4RAo",
+  authDomain: "think-tech-60779.firebaseapp.com",
+  databaseURL: "https://think-tech-60779-default-rtdb.firebaseio.com",
+  projectId: "think-tech-60779",
+  storageBucket: "think-tech-60779.appspot.com",
+  messagingSenderId: "594773938377",
+  appId: "1:594773938377:web:318ca45db0fa3c03a23d18",
+  measurementId: "G-BE1X2279Y3"
 });
 
 const db = app.firestore();
